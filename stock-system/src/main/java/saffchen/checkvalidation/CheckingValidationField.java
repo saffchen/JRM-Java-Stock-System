62b5447e320ac2a8233bc92e85542764971aea73
Денис <denis.savchenko98@yandex.ru>
2022-05-25 23:08:07 +0300

    Validation

--- stock-system/src/main/java/saffchen/checkvalidation/CheckingValidationField.java
+++ stock-system/src/main/java/saffchen/checkvalidation/CheckingValidationField.java
@@ -15,29 +15,32 @@ public class CheckingValidationField {
                 .configure()
                 .buildValidatorFactory();
         Validator validator = validatorFactory.getValidator();
+        try {
+            System.out.println("*** ADDITIONAL PRODUCT ***");
+            System.out.print("Укажите название продукта: ");
+            String title = new Scanner(System.in).next();
+            System.out.print("Укажите описание продукта: ");
+            String description = new Scanner(System.in).next();
+            System.out.print("Укажите цену продукта: ");
+            int price = new Scanner(System.in).nextInt();
+            System.out.print("Введите теги: ");
+            String[] tags = new Scanner(System.in).next().trim().split(",");
+            System.out.print("Укажите категорию продукта: ");
+            String category = new Scanner(System.in).next().trim();
+            System.out.print("Укажите количество продукта: ");
+            int count = new Scanner(System.in).nextInt();
+            System.out.print("Укажите склад на котором хранится продукт: ");
+            String satellite = new Scanner(System.in).next().toUpperCase(Locale.ROOT);
 
-        System.out.println("*** ADDITIONAL PRODUCT ***");
-        System.out.print("Укажите название продукта: ");
-        String title = new Scanner(System.in).next();
-        System.out.print("Укажите описание продукта: ");
-        String description = new Scanner(System.in).next();
-        System.out.print("Укажите цену продукта: ");
-        int price = new Scanner(System.in).nextInt();
-        System.out.print("Введите теги: ");
-        String[] tags = new Scanner(System.in).next().trim().split(",");
-        System.out.print("Укажите категорию продукта: ");
-        String category = new Scanner(System.in).next().trim();
-        System.out.print("Укажите количество продукта: ");
-        int count = new Scanner(System.in).nextInt();
-        System.out.print("Укажите склад на котором хранится продукт: ");
-        String satellite = new Scanner(System.in).next().toUpperCase(Locale.ROOT);
+            Product additionalProduct = new Product(title, description, price, List.of(tags), category,
+                    count, satellite);
 
-        Product additionalProduct = new Product(title, description, price, List.of(tags), category,
-                count, satellite);
-
-        Set<ConstraintViolation<Product>> violations = validator.validate(additionalProduct);
-        for (ConstraintViolation<Product> warning : violations) {
-            System.out.println(warning.getMessage());
+            Set<ConstraintViolation<Product>> violations = validator.validate(additionalProduct);
+            for (ConstraintViolation<Product> warning : violations) {
+                System.out.println(warning.getMessage());
+            }
+        } catch (Exception e) {
+            System.out.println("Вы ввели некорректное значение");
         }
     }
 }
