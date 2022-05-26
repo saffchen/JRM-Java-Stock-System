5774040b2d87629a6f79c50da7de5e27c9f80ba1
Денис <denis.savchenko98@yandex.ru>
2022-05-26 20:39:17 +0300

    Refactoring by feedback

--- stock-system/src/main/java/saffchen/command/ShowAllCommand.java
+++ stock-system/src/main/java/saffchen/command/ShowAllCommand.java
@@ -1,13 +1,8 @@
 package saffchen.command;
 
-import saffchen.database.FileConnection;
-import saffchen.utils.FileStorageUtils;
-
 public class ShowAllCommand implements Command {
     @Override
     public void doCommand() {
-        FileConnection fileConnection = FileConnection.getInstance("stock_import_csv.csv");
-        FileStorageUtils fileStorageUtils = new FileStorageUtils(fileConnection);
-        fileStorageUtils.showAllProducts();
+
     }
 }
