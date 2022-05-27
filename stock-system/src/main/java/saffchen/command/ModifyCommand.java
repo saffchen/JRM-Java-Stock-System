7b26009091acceb415e9ace257d81752512f9e12
Vitaly Savich <freesky15@yandex.ru>
2022-05-27 15:25:20 +0500

    Refactoring

--- stock-system/src/main/java/saffchen/command/ModifyCommand.java
+++ stock-system/src/main/java/saffchen/command/ModifyCommand.java
@@ -17,7 +17,7 @@ public class ModifyCommand implements Command {
     public void doCommand() {
         FileConnection fileConnection = FileConnection.getInstance("stock_import_csv.csv");
         FileStorageUtils fileStorageUtils = new FileStorageUtils(fileConnection);
-        fileStorageUtils.modifyProduct(new Product("Test record",
+        fileStorageUtils.modifyProduct(new Product("Test record Modified",
                         "Description for test record",
                         1200d,
                         Arrays.asList("test1 tag", "test2 tag"),
