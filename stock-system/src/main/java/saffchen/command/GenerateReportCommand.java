2fa1ded0bc6cd2fc9b35b879510c699eefc659c1
Vitaly Savich <freesky15@yandex.ru>
2022-05-27 19:09:10 +0500

    Refactoring

--- stock-system/src/main/java/saffchen/command/GenerateReportCommand.java
+++ stock-system/src/main/java/saffchen/command/GenerateReportCommand.java
@@ -22,8 +22,8 @@ public class GenerateReportCommand implements Command {
                 FileConnection.getInstance("stock_import_csv.csv"));
 
         String criteria;
-        String header = "";
-        while (MenuUtils.isExit(header)) {
+        String header;
+        while (true) {
             System.out.println("*** REPORT ***");
             System.out.println("Possible values: ");
 
@@ -32,18 +32,21 @@ public class GenerateReportCommand implements Command {
 
             System.out.print("\nEnter the field to search or EXIT: ");
             header = scanner.next().trim().toUpperCase();
+            if (header.equals("EXIT"))
+                break;
 
-            while (MenuUtils.isExit(header)) {
+            while (true) {
                 System.out.println("*** Searching  by " + header + " ***");
                 System.out.print("Enter the KEYWORD or EXIT: ");
                 criteria = scanner.next().trim().toUpperCase();
-
+                if (criteria.equals("EXIT"))
+                    break;
                 try {
                     header = header.substring(0, 1) + header.substring(1, header.length()).toLowerCase();
                     PDFReportFromFile report = new PDFReportFromFile(header, criteria);
                     report.generateReport();
                 } catch (Exception e) {
-                    System.out.println("Error: Can't create the report! Enter correct criteries and try again!");
+                    System.out.println("Error: Can't create the report! Try again!");
                 }
             }
         }
