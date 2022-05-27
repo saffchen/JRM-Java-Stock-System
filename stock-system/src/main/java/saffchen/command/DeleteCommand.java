43cb7486538c97acd6a149168d7d1b008414254e
Vitaly Savich <freesky15@yandex.ru>
2022-05-27 14:54:12 +0500

    Refactoring

--- stock-system/src/main/java/saffchen/command/DeleteCommand.java
+++ stock-system/src/main/java/saffchen/command/DeleteCommand.java
@@ -1,14 +1,29 @@
 package saffchen.command;
 
+import saffchen.database.FileConnection;
+import saffchen.product.Product;
+import saffchen.utils.FileStorageUtils;
+
+import java.util.Arrays;
+
 public class DeleteCommand implements Command {
 
-    @Override
-    public String getInfo() {
+    public String getInfo(){
         return "4 Write an \"delete_product\" if you want to delete product";
     }
 
     @Override
     public void doCommand() {
-
+        FileConnection fileConnection = FileConnection.getInstance("stock_import_csv.csv");
+        FileStorageUtils fileStorageUtils = new FileStorageUtils(fileConnection);
+        fileStorageUtils.deleteProduct(new Product("Test record",
+                        "Description for test record",
+                        1200d,
+                        Arrays.asList("test1 tag", "test2 tag"),
+                        "test category",
+                        6,
+                        "Ekaterinburg"
+                )
+        );
     }
 }
