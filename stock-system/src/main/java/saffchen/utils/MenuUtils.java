fa03d964d5a064755229f53ebbabedf3ec10bf6f
Денис <denis.savchenko98@yandex.ru>
2022-05-31 21:08:24 +0300

    Refactoring by feedback. Method isExit has been simpify.

--- stock-system/src/main/java/saffchen/utils/MenuUtils.java
+++ stock-system/src/main/java/saffchen/utils/MenuUtils.java
@@ -7,10 +7,11 @@ public class MenuUtils {
     private static final List<String> POSITIVE_ANSWERS = List.of("ДА", "YES");
 
     public static boolean isExit(String keyword) {
-        return !(keyword.trim().toUpperCase().equals("EXIT")) ? true : false;
+        return "EXIT". equalsIgnoreCase(keyword.trim());
     }
 
     public static boolean isYes(String keyword) {
         return POSITIVE_ANSWERS.contains(keyword.trim().toUpperCase()) ? true : false;
     }
 }
+
