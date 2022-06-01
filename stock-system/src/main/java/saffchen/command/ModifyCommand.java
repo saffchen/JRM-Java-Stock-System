498fa7a307401c294809ee78d5320a03c32d7cec
Aleksandr Alekseenko <alexan.sergeev@gmail.com>
2022-06-01 18:57:49 -0400

    Rafactoring ModifyCommand

--- stock-system/src/main/java/saffchen/command/ModifyCommand.java
+++ stock-system/src/main/java/saffchen/command/ModifyCommand.java
@@ -3,7 +3,6 @@ package saffchen.command;
 import saffchen.database.FileConnection;
 import saffchen.product.Product;
 import saffchen.utils.FileStorageUtils;
-import saffchen.utils.MenuUtils;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -22,6 +21,8 @@ import static saffchen.utils.ValidationUtil.isStringToIntegerValid;
 
 public class ModifyCommand implements Command {
 
+    private BufferedReader bufferedReader;
+
     @Override
     public String getInfo() {
         return "Write an \"modify_product\" if you want to make changes";
@@ -30,7 +31,7 @@ public class ModifyCommand implements Command {
     @Override
     public void doCommand() throws IOException {
 
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         String inputString = "";
 
 
@@ -38,9 +39,9 @@ public class ModifyCommand implements Command {
             System.out.println("Введите имя продукта, который вы хотите изменить/Please, input the product name for update");
             System.out.println("Или введите Exit для выхода из этой команды/Either input Exit to exit from modify_product");
             System.out.print("Имя продукта/The product name is: ");
-            inputString = bufferedReader.readLine().trim();
+            inputString = bufferedReader.readLine()
+                                        .trim();
 
-            //FileConnection fileConnection = FileConnection.getInstance("stock_import_csv.csv");
             FileStorageUtils fileStorageUtils = new FileStorageUtils(FileConnection.getInstance("stock_import_csv.csv"));
             Product product = fileStorageUtils.getProductByTitle(inputString);
             Field[] fields = Product.class.getDeclaredFields();
@@ -54,35 +55,27 @@ public class ModifyCommand implements Command {
                     Field field = fields[x];
                     String fieldName = field.getName();
                     System.out.print(String.format("Please, enter a new %s: ", fieldName.toUpperCase()));
-                    inputString = bufferedReader.readLine().trim();
+                    inputString = bufferedReader.readLine()
+                                                .trim();
                     if (inputString.equals("") || isExit(inputString)) {
+                        // Converting the List to String with a "\\s" delimiter
                         if (fieldName.equals("tags")) {
                             inputString = ((List<String>) runGetter(field, product)).stream()
-                                    .map(String::valueOf)
-                                    .collect(Collectors.joining(" "));
+                                                                                    .map(String::valueOf)
+                                                                                    .collect(Collectors.joining(" "));
                         } else {
                             inputString = getInputString(field, product);
                         }
+                        // Price validation
                     } else if (fieldName.equals("price") && !isStringToDoubleValid(inputString)) {
                         do {
-                            System.out.println(String.format("Пожалуйста, введите корректную %s. %<s может состоять только из цифр./" +
-                                    "Please input a correct %<s. %<s can consist the digits only", fieldName.toUpperCase()));
-                            System.out.print(String.format("Please, enter a new %s: ", fieldName.toUpperCase()));
-                            inputString = bufferedReader.readLine().trim();
-                            if (inputString.equals("") || isExit(inputString)) {
-                                inputString = getInputString(field, product);
-                            }
+                            inputString = inputCorrectValue(field, product);
                         }
                         while (!isDoubleValidOrExit(inputString));
+                        // Count validation
                     } else if (fieldName.equals("count") && !isStringToIntegerValid(inputString)) {
                         do {
-                            System.out.println(String.format("Пожалуйста, введите корректное %s. %<s может состоять только из цифр./" +
-                                    "Please input a correct %<s. %<s can consist the digits only", fieldName.toUpperCase()));
-                            System.out.print(String.format("Please, enter a new %s: ", fieldName.toUpperCase()));
-                            inputString = bufferedReader.readLine().trim();
-                            if (inputString.equals("") || isExit(inputString)) {
-                                inputString = getInputString(field, product);
-                            }
+                            inputString = inputCorrectValue(field, product);
                         } while (!isIntegerValidOrExit(inputString));
                     }
                     newFieldsMap.put(fieldName, inputString);
@@ -93,15 +86,37 @@ public class ModifyCommand implements Command {
         } while (!isExit(inputString));
     }
 
-    // https://stackoverflow.com/questions/13400075/reflection-generic-get-field-value
+    private String inputCorrectValue(Field field, Product product) throws IOException {
+        String fieldName = field.getName()
+                                .toUpperCase();
+        String inputString;
+        System.out.println(String.format("Пожалуйста, введите корректную %s. %<s может состоять только из цифр./" +
+                "Please input a correct %<s. %<s can consist the digits only", fieldName));
+        System.out.print(String.format("Please, enter a new %s: ", fieldName));
+        inputString = bufferedReader.readLine()
+                                    .trim();
+        if (inputString.equals("") || isExit(inputString)) {
+            inputString = getInputString(field, product);
+        }
+        return inputString;
+    }
+
     private String getInputString(Field field, Product product) {
         return String.valueOf(runGetter(field, product));
     }
 
+    // https://stackoverflow.com/questions/13400075/reflection-generic-get-field-value
     private Object runGetter(Field field, Product product) {
-        for (Method method : product.getClass().getMethods()) {
-            if ((method.getName().startsWith("get")) && (method.getName().length() == (field.getName().length() + 3))) {
-                if (method.getName().toLowerCase().endsWith(field.getName().toLowerCase())) {
+        for (Method method : product.getClass()
+                                    .getMethods()) {
+            if ((method.getName()
+                       .startsWith("get")) && (method.getName()
+                                                     .length() == (field.getName()
+                                                                        .length() + 3))) {
+                if (method.getName()
+                          .toLowerCase()
+                          .endsWith(field.getName()
+                                         .toLowerCase())) {
                     try {
                         return method.invoke(product);
                     } catch (IllegalAccessException | InvocationTargetException e) {
