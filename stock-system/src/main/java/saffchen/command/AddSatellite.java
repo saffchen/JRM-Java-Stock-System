aa8004f2c4f2d9f713f524c173ee5e29ac27004d
Денис <denis.savchenko98@yandex.ru>
2022-06-11 20:50:49 +0300

    Debug. By saffchen

--- stock-system/src/main/java/saffchen/command/AddSatellite.java
+++ stock-system/src/main/java/saffchen/command/AddSatellite.java
@@ -2,7 +2,6 @@ package saffchen.command;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
@@ -12,8 +11,7 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class AddSatellite implements Command {
-    private static final Logger logger
-            = LoggerFactory.getLogger(AddSatellite.class);
+
     @Override
     public String getInfo() {
         return "Write an \"add_satellite\" if you want to additional satellite";
@@ -21,17 +19,15 @@ public class AddSatellite implements Command {
 
     @Override
     public void doCommand() throws GeneralSecurityException, IOException {
-        logger.info(" --- ADD_SATELLITE ---");
-        while (true) {
-            System.out.println("Чтобы выйти из режима добавления склада введите команду exit");
-            System.out.print("Введите название нового склада: ");
-            String str = new Scanner(System.in).nextLine().toUpperCase(Locale.ROOT);
-            if (str.equals("EXIT")) break;
-            FileWriter fileWriter = new FileWriter(new File(System.getProperty("user.dir"), "satellite.txt"));
-            fileWriter.append("\n");
-            fileWriter.append(str);
-            fileWriter.close();
-            logger.info(" --- ADD_SATELLITE --- {{}}", str);
+            while (true) {
+                System.out.println("Чтобы выйти из режима добавления склада введите команду exit");
+                System.out.print("Введите название нового склада: ");
+                String str = new Scanner(System.in).nextLine().toUpperCase(Locale.ROOT);
+                if (str.equals("EXIT")) break;
+                BufferedWriter writer = new BufferedWriter(new FileWriter("satellite.txt", true));
+                writer.append("\n");
+                writer.append(str);
+                writer.close();
+            }
         }
     }
-}
