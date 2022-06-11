cceee4a6802bc75abf48e9d3a4ea77fbaa8ca310
Денис <denis.savchenko98@yandex.ru>
2022-06-11 20:53:50 +0300

    Debug. By saffchen

--- stock-system/src/main/java/saffchen/command/AddSatellite.java
+++ stock-system/src/main/java/saffchen/command/AddSatellite.java
@@ -2,6 +2,7 @@ package saffchen.command;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
@@ -11,6 +12,8 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class AddSatellite implements Command {
+    private static final Logger logger
+            = LoggerFactory.getLogger(AddCommand.class);
 
     @Override
     public String getInfo() {
@@ -19,15 +22,25 @@ public class AddSatellite implements Command {
 
     @Override
     public void doCommand() throws GeneralSecurityException, IOException {
-            while (true) {
-                System.out.println("Чтобы выйти из режима добавления склада введите команду exit");
-                System.out.print("Введите название нового склада: ");
-                String str = new Scanner(System.in).nextLine().toUpperCase(Locale.ROOT);
-                if (str.equals("EXIT")) break;
-                BufferedWriter writer = new BufferedWriter(new FileWriter("satellite.txt", true));
+        logger.info(" --- ADD_SATELLITE --- ");
+        while (true) {
+            System.out.println("Чтобы выйти из режима добавления склада введите команду exit");
+            System.out.print("Введите название нового склада: ");
+            String str = new Scanner(System.in).nextLine().toUpperCase(Locale.ROOT);
+            if (str.equals("EXIT")) break;
+            BufferedWriter writer = null;
+            try {
+                writer = new BufferedWriter(new FileWriter("satellite.txt", true));
                 writer.append("\n");
                 writer.append(str);
+                writer.close();
+                logger.info(" --- ADD_SATELLITE --- {{}}", str);
+            } catch (IOException e){
+                throw new RuntimeException(e);
+            } finally {
+                assert writer != null;
                 writer.close();
             }
         }
     }
+}
