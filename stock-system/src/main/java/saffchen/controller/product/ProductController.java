2bc9733a81a73212ca192a51f8efc5da1b155bb1
Denis Savchenko <denis.savchenko98@yandex.ru>
2022-07-06 19:49:00 +0300

    Create product REST API

--- stock-system/src/main/java/saffchen/controller/product/ProductController.java
+++ stock-system/src/main/java/saffchen/controller/product/ProductController.java
@@ -1,14 +1,17 @@
 package saffchen.controller.product;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import saffchen.entities.ProductEntity;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @RestController
@@ -18,10 +21,17 @@ public class ProductController extends AbstractProductController {
 
     static final String REST_URL = "/api/products";
 
-    @Override
+    @Autowired
+    private final ProductMapper productMapper;
+
     @GetMapping
-    public ResponseEntity<List<ProductEntity>> getAll() {
-        log.info("getAll");
-        return super.getAll();
+    public ResponseEntity<List<ProductDto>> getAllProducts(){
+        return ResponseEntity.ok(productMapper.toProductsDtoList(productService.getAllProducts()));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductDto> getProductById(@PathVariable Long id) {
+        Optional<ProductEntity> product = productService.getProductById(id);
+        return ResponseEntity.ok(productMapper.productToProductDto(product.get()));
     }
 }
