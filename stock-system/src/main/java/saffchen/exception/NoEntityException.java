a7a864c245fd402d5b127fef2373d8bee2183c5d
Denis Savchenko <96635470+saffchen@users.noreply.github.com>
2022-07-07 23:54:34 +0300

    Update NoEntityException.java

--- stock-system/src/main/java/saffchen/exception/NoEntityException.java
+++ stock-system/src/main/java/saffchen/exception/NoEntityException.java
@@ -4,8 +4,6 @@ package saffchen.exception;
  * @author saffchen created on 07.07.2022
  * @project JRM-Java-Stock-System
  */
-public class NoEntityException extends Exception{
-    public NoEntityException(Long id) {
-        System.out.printf("Object {%s} is not found", id);
-    }
+public NoEntityException(String messageError) {
+        super(messageError);
 }
