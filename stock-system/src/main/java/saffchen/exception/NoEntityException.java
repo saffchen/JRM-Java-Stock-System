b2479d00372cf1db1a703829118cc3e99b28c8cb
Denis Savchenko <96635470+saffchen@users.noreply.github.com>
2022-07-08 19:51:29 +0300

    Update NoEntityException.java

--- stock-system/src/main/java/saffchen/exception/NoEntityException.java
+++ stock-system/src/main/java/saffchen/exception/NoEntityException.java
@@ -4,6 +4,8 @@ package saffchen.exception;
  * @author saffchen created on 07.07.2022
  * @project JRM-Java-Stock-System
  */
-public NoEntityException(String messageError) {
+public class NoEntityException extends Exception{
+    public NoEntityException(String messageError) {
         super(messageError);
+    }
 }
