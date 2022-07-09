80a90baabea0d1fad6da542c316add9875e9e164
Denis Savchenko <denis.savchenko98@yandex.ru>
2022-07-09 16:14:51 +0300

    debug

--- stock-system/src/main/java/saffchen/dto/SatelliteDTO.java
+++ stock-system/src/main/java/saffchen/dto/SatelliteDTO.java
@@ -1,17 +1,11 @@
 package saffchen.dto;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.*;
-import saffchen.entities.ProductEntity;
-
-import javax.persistence.*;
-import java.util.List;
 @Data
 @Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-@ToString
 public class SatelliteDTO {
     private long id;
     private String name;
