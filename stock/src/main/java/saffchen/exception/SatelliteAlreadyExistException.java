ade4d989cf718998edc3158203221c15163c020c
Denis Savchenko <denis.savchenko98@yandex.ru>
2022-07-18 23:58:59 +0300

    minor update

--- stock/src/main/java/saffchen/exception/SatelliteAlreadyExistException.java
+++ stock/src/main/java/saffchen/exception/SatelliteAlreadyExistException.java
@@ -4,7 +4,7 @@ package saffchen.exception;
  * @author saffchen created on 16.07.2022
  */
 public class SatelliteAlreadyExistException extends RuntimeException {
-    public SatelliteAlreadyExistException(String message) {
-        super(message);
+    public SatelliteAlreadyExistException(String messageError) {
+        super(messageError);
     }
 }
