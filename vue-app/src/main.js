20e71c45613f781f78954c64f4eb2f2107439632
Denis Savchenko <denis.savchenko98@yandex.ru>
2022-07-20 22:16:13 +0300

    Added login page

--- vue-app/src/main.js
+++ vue-app/src/main.js
@@ -4,8 +4,10 @@ import HomePage from './HomePage';
 
 import ApiPlugin from './plugins/api';
 import LoadPlugin from './plugins/load';
+import LoginPage from "@/LoginPage";
 
 createApp(HomePage)
+    .use(LoginPage)
     .use(ApiPlugin)
     .use(LoadPlugin)
     .mount('#app');
