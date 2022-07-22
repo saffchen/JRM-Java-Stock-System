661ed272698d69749199418721cd1b0a89a6537d
Denis Savchenko <denis.savchenko98@yandex.ru>
2022-07-22 21:05:22 +0300

    Added login page

--- vue-app/src/components/main/Login.vue
+++ vue-app/src/components/main/Login.vue
@@ -4,23 +4,33 @@
     <StockTable/>
   </main>
   <form action=/login method="post">
-    <div align="center">
-      <label> User Name:</label>
-      <input type="text" name="username"/>
-    </div>
-    <div>
-      <label></label>
-    </div>
-    <div align="center">
-      <label> Password:</label>
-      <input type="password" name="password"/>
-    </div>
-    <div>
-      <label></label>
-    </div>
-    <div align="center">
-      <input type="submit" value="Sign In"/>
-    </div>
+    <div class="container">
+      <div class="row" align="center">
+        <div class="col">
+          User Name:
+          <div class="col">
+            <input type="text" name="username"/>
+          </div>
+        </div>
+      </div>
+          <div>
+            <label></label>
+          </div>
+          <div class="row" align="center">
+            <div class="col">
+              Password:
+              <div class="col">
+                <input type="password" name="password"/>
+              </div>
+              <div>
+              </div>
+                <label></label>
+              </div>
+              <div align="center">
+                <input type="submit" value="Sign In"/>
+              </div>
+            </div>
+          </div>
   </form>
 </template>
 
