0c3c87d0c1a4da3151dad243152becfdff453626
Denis Savchenko <denis.savchenko98@yandex.ru>
2022-07-22 21:05:33 +0300

    Added login button

--- vue-app/src/components/header/LoginBtn.vue
+++ vue-app/src/components/header/LoginBtn.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="d-flex align-items-center justify-content-end">
-    <div class="me-3">Push to login in the system</div>
     <button @click="login" class="btn btn-outline-primary">Login</button>
   </div>
 </template>
--- vue-app/src/components/header/Navbar.vue
+++ vue-app/src/components/header/Navbar.vue
@@ -1,27 +1,14 @@
 <template>
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-        <div class="container justify-content-between">
-            <DropdownMenu/>
-            <div class="navbar-brand fs-4">JRM Stock System Project</div>
-            <LoginBtn/>
-        </div>
-    </nav>
-
-//Проверить новый вариант
-<div class="container">
-  <div class="row justify-content-md-center">
-    <div class="col col-lg-2">
-      <DropdownMenu/>
-    </div>
-    <div class="col-md-auto">
-      <div class="navbar-brand fs-4">JRM Stock System Project</div>
-    </div>
-    <div class="col col-lg-2">
-      <LoginBtn/>
-    </div>
-  </div>
-
+      <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+          <div class="container justify-content-between">
+              <DropdownMenu/>
+              <div class="navbar-brand col-6" align="center">
+                  JRM Stock System Project
+              </div>
+              <LoginBtn/>
+          </div>
+      </nav>
 
 </template>
 
@@ -30,14 +17,14 @@ import DropdownMenu from "./DropdownMenu.vue";
 import LoginBtn from "./LoginBtn.vue";
 
 export default {
-    components: {
-        DropdownMenu, InvisibleBtn
-    }
+  components: {
+    DropdownMenu, LoginBtn
+  }
 }
- 
- //https://bootstrap-4.ru/docs/4.0/layout/grid/
- //https://getbootstrap.com/docs/5.0/components/navbar/?
- 
+
+//https://bootstrap-4.ru/docs/4.0/layout/grid/
+//https://getbootstrap.com/docs/5.0/components/navbar/?
+
 
 </script>
 
