c89de2176be15df82c7c94490cf81c4c112653ae
Denis Savchenko <denis.savchenko98@yandex.ru>
2022-07-31 14:13:12 +0300

    delete unused dependency

--- vue-app/src/views/LoginPage.vue
+++ vue-app/src/views/LoginPage.vue
@@ -8,7 +8,6 @@
 <script>
 import Header from "@/components/header/Header";
 import Main from "@/components/main/Main";
-import Footer from "@/components/footer/Footer";
 
 export default {
   name: 'app',
