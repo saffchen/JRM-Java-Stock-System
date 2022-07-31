d9f18d7ed5b26d868e9214280bd13a5a40d08ff4
Denis Savchenko <denis.savchenko98@yandex.ru>
2022-07-31 17:05:08 +0300

    Add login, logout logic

--- vue-app/src/components/header/Navbar.vue
+++ vue-app/src/components/header/Navbar.vue
@@ -1,24 +1,25 @@
-<template>
+<template xmlns="http://www.w3.org/1999/html">
 
-      <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-          <div class="container justify-content-between">
-              <DropdownMenu/>
-              <div class="navbar-brand col-6" align="center">
-                  JRM Stock System Project
-              </div>
-              <LoginBtn/>
-          </div>
-      </nav>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+    <div class="container justify-content-between">
+      <DropdownMenu/>
+      <div class="navbar-brand col-6" align="center">
+        JRM Stock System Project
+      </div>
+      <LoginBtn/>
+    </div>
+  </nav>
 
 </template>
 
 <script>
 import DropdownMenu from "./DropdownMenu.vue";
 import LoginBtn from "./LoginBtn.vue";
+import LogoutBtn from "./LogoutBtn";
 
 export default {
   components: {
-    DropdownMenu, LoginBtn
+    DropdownMenu, LoginBtn, LogoutBtn
   }
 }
 
