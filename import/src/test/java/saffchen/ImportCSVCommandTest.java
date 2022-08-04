21a67f091235114b557f91825781b9976f803cf2
Denis Savchenko <denis.savchenko98@yandex.ru>
2022-08-04 21:38:37 +0300

    JT-38. Add ImportCSVCommandTest

---
+++ import/src/test/java/saffchen/ImportCSVCommandTest.java
@@ -0,0 +1,25 @@
+package saffchen;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Objects;
+
+/**
+ * @author saffchen created on 03.08.2022
+ */
+
+class ImportCSVCommandTest {
+
+    @Test
+    void doCommand() throws IOException {
+        StringBuilder stringBuilder = new StringBuilder();
+        ClassLoader classLoader = getClass().getClassLoader();
+        FileReader reader = new FileReader((Objects.requireNonNull(classLoader.getResource("stock_import_csv.csv.txt"))).getFile());
+        while (reader.ready()) {
+            stringBuilder.append((char) reader.read());
+        }
+        System.out.println(stringBuilder);
+    }
+}
