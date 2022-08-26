76fe32c11f0f5e768d8447c3f4f1a6233c70cca0
Denis Savchenko <denis.savchenko98@yandex.ru>
2022-08-26 20:10:30 +0300

    Refactoring api path

--- vue-app/src/api/instance.js
+++ vue-app/src/api/instance.js
@@ -7,7 +7,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8080/api/v1',
+    baseURL: 'http://localhost:8080/api/v1/',
     withCredentials: false,
     headers: {
         accept: 'application/json'
--- vue-app/src/api/security.js
+++ vue-app/src/api/security.js
@@ -1,7 +1,7 @@
 export default function (instance) {
     return {
         getAll() {
-            return instance.get('auth/check_auth/')
+            return instance.get('/auth/check_auth/')
         }
        }
       }
