ed51fd37d2c4d34a115191b48a000946b05c5c60
Aleksandr Alekseenko <alexan.sergeev@gmail.com>
2022-09-05 14:31:12 -0400

    JT-44 SQLException instead of ConstraintViolationException handler

--- stock/src/main/java/saffchen/error/GlobalExceptionHandler.java
+++ stock/src/main/java/saffchen/error/GlobalExceptionHandler.java
@@ -1,8 +1,7 @@
-package saffchen.controller;
+package saffchen.error;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.boot.web.error.ErrorAttributeOptions;
 import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.http.HttpHeaders;
@@ -12,15 +11,14 @@ import org.springframework.lang.NonNull;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-import saffchen.error.AppException;
-import saffchen.error.DataConflictException;
-import saffchen.exception.NoEntityException;
 import saffchen.util.validation.ValidationUtil;
 
 import javax.persistence.EntityNotFoundException;
+import java.sql.SQLException;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -30,73 +28,6 @@ import static org.springframework.boot.web.error.ErrorAttributeOptions.Include.M
  * @author saffchen created on 16.07.2022
  * https://www.bezkoder.com/spring-boot-restcontrolleradvice/
  */
-/*@RestControllerAdvice
-@AllArgsConstructor
-@Slf4j
-
-public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
-    private final ErrorAttributes errorAttributes;
-
-    @ExceptionHandler(AppException.class)
-    public ResponseEntity<?> appException(WebRequest request, AppException ex) {
-        log.error("ApplicationException: {}", ex.getMessage());
-        return createResponseEntity(request, ex.getOptions(), null, ex.getStatus());
-    }
-
-    @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<?> entityNotFoundException(WebRequest request, EntityNotFoundException ex) {
-        log.error("EntityNotFoundException: {}", ex.getMessage());
-        return createResponseEntity(request, ErrorAttributeOptions.of(MESSAGE), null, HttpStatus.UNPROCESSABLE_ENTITY);
-    }
-
-    @NonNull
-    @Override
-    protected ResponseEntity<Object> handleExceptionInternal(
-            @NonNull Exception ex, Object body, @NonNull HttpHeaders headers, @NonNull HttpStatus status, @NonNull WebRequest request) {
-        log.error("Exception", ex);
-        log.info("Exception", ex);
-        super.handleExceptionInternal(ex, body, headers, status, request);
-        return createResponseEntity(request, ErrorAttributeOptions.of(), ValidationUtil.getRootCause(ex).getMessage(), status);
-    }
-
-    @NonNull
-    @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(
-            MethodArgumentNotValidException ex,
-            @NonNull HttpHeaders headers, @NonNull HttpStatus status, @NonNull WebRequest request) {
-        return handleBindingErrors(ex.getBindingResult(), request);
-    }
-
-    @NonNull
-    @Override
-    protected ResponseEntity<Object> handleBindException(
-            BindException ex, @NonNull HttpHeaders headers, @NonNull HttpStatus status, @NonNull WebRequest request) {
-        return handleBindingErrors(ex.getBindingResult(), request);
-    }
-
-    private ResponseEntity<Object> handleBindingErrors(BindingResult result, WebRequest request) {
-        String msg = result.getFieldErrors().stream()
-                           .map(fe -> String.format("[%s] %s", fe.getField(), fe.getDefaultMessage()))
-                           .collect(Collectors.joining("\n"));
-        return createResponseEntity(request, ErrorAttributeOptions.defaults(), msg, HttpStatus.UNPROCESSABLE_ENTITY);
-    }
-
-    @SuppressWarnings("unchecked")
-    private <T> ResponseEntity<T> createResponseEntity(WebRequest request, ErrorAttributeOptions options, String msg, HttpStatus status) {
-        Map<String, Object> body = errorAttributes.getErrorAttributes(request, options);
-        if (msg != null) {
-            body.put("message", msg);
-        }
-        body.put("status", status.value());
-        body.put("error", status.getReasonPhrase());
-        return (ResponseEntity<T>) ResponseEntity.status(status).body(body);
-    }
-}
-*/
-
-
-
-
 @RestControllerAdvice
 @AllArgsConstructor
 @Slf4j
@@ -139,9 +70,9 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         return createResponseEntity(getDefaultBody(request, ex.getOptions(), null), ex.getStatus());
     }
 
-    @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<?> persistException(WebRequest request, ConstraintViolationException ex) {
-        log.error("ConstraintViolationException: {}", ex.getMessage());
+    @ExceptionHandler(SQLException.class)
+    public ResponseEntity<?> handeSQLException(WebRequest request, SQLException ex) {
+        log.error("SQLException: {}", ex.getMessage());
         return createResponseEntity(getDefaultBody(request, ErrorAttributeOptions.of(MESSAGE), null), HttpStatus.UNPROCESSABLE_ENTITY);
     }
 
@@ -187,8 +118,7 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         log.error("Exception", ex);
         log.info("Exception", ex);
         super.handleExceptionInternal(ex, body, headers, status, request);
-        return createResponseEntity(getDefaultBody(request, ErrorAttributeOptions.of(), ValidationUtil.getRootCause(ex).getMessage()), status);
+        return createResponseEntity(getDefaultBody(request, ErrorAttributeOptions.of(), ValidationUtil.getRootCause(ex)
+                                                                                                      .getMessage()), status);
     }
-
-
 }
