d69db4420bd30b5a67efd4ebfdb8fc5cd72ac1e3
Denis Savchenko <96635470+saffchen@users.noreply.github.com>
2022-10-09 11:10:34 +0300

    Delete Main.java

--- report/src/main/java/saffchen/Main.java
+++
@@ -1,9 +0,0 @@
-package saffchen;
-
-import saffchen.reports.GenerateReportCommand;
-
-public class Main {
-    public static void main(String[] args) throws Exception {
-       new GenerateReportCommand().doCommand();
-    }
-}
