e07f9613ebbae8b265a03d5055dd7dc8c42526fa
denis <denis.savchenko98@yandex.ru>
2022-11-17 22:30:38 +0300

    Refactoring

--- import/src/main/java/saffchen/utils/FileUtils.java
+++ import/src/main/java/saffchen/utils/FileUtils.java
@@ -7,38 +7,6 @@ import java.lang.reflect.Field;
 import java.util.*;
 
 public class FileUtils {
-    public static List<String> getSatelliteList() {
-        return Collections.singletonList(getInfoFromTxtFile("satellite.txt"));
-    }
-
-    public static String getSatelliteString() {
-        return getInfoFromTxtFile("satellite.txt");
-    }
-
-    public static List<saffchen.database.User> getUsersFromFile(String path) {
-        List<User> users = new ArrayList<>();
-        String str = null;
-        if (path == null || path.isEmpty())
-            return users;
-        try (Scanner reader = new Scanner(new InputStreamReader(
-                FileUtils.class.getResourceAsStream("/" + path)))) {
-            while (reader.hasNext()) {
-                String[] splitedStr = reader.nextLine().trim().split(";");
-                User user = new User();
-                Field[] fields = user.getClass().getDeclaredFields();
-                for (int i = 0; i < splitedStr.length; i++) {
-                    fields[i].setAccessible(true);
-                    fields[i].set(user, splitedStr[i]);
-                }
-                users.add(user);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println("Error: Unknown error");
-        }
-        return users;
-    }
-
     public static String getInfoFromTxtFile(String filePath) {
         StringBuilder builder = new StringBuilder();
         try (FileReader reader = new FileReader(filePath)) {
